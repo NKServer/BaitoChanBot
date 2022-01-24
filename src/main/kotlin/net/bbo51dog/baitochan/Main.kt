40fcd3e0d78ec9d8bd4e3b10ee29313ca734e9e0
bbo51dog <bbo51dog@gmail.com>
2022-01-25 03:15:47 +0900

    Show players to activity

--- src/main/kotlin/net/bbo51dog/baitochan/Main.kt
+++ src/main/kotlin/net/bbo51dog/baitochan/Main.kt
@@ -1,10 +1,15 @@
 package net.bbo51dog.baitochan
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jagrosh.jdautilities.command.CommandClientBuilder
 import net.bbo51dog.baitochan.command.StatusCommand
-import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 
 const val TOKEN_ENV = "BAITO_CHAN_TOKEN"
 const val COMMAND_PREFIX = "bc!"
@@ -13,11 +18,23 @@ fun main() {
     val commandClient = CommandClientBuilder()
         .setOwnerId("")
         .setPrefix(COMMAND_PREFIX)
-        .setActivity(Activity.playing("nkserver.net"))
         .addCommand(StatusCommand())
         .build()
 
-    JDABuilder.createDefault(System.getenv(TOKEN_ENV))
+    val jda = JDABuilder.createDefault(System.getenv(TOKEN_ENV))
         .addEventListeners(commandClient)
         .build()
+
+    Executors.newScheduledThreadPool(1).scheduleAtFixedRate(
+        {
+            val connection = URL(StatusCommand.URL_BASE + StatusCommand.ADDRESS).openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.connect()
+            val data = jacksonObjectMapper().readTree(connection.inputStream)
+            jda.presence.activity = Activity.playing(COMMAND_PREFIX + " - " + data.get("players").get("online").asText() + " Online Players")
+        },
+        0,
+        1,
+        TimeUnit.MINUTES
+    )
 }

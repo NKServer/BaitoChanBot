d4203495fbf25c325af56a27ac9dab9f19b0bf9f
bbo51dog <bbo51dog@gmail.com>
2022-01-25 19:09:44 +0900

    Activity

--- src/main/kotlin/net/bbo51dog/baitochan/Main.kt
+++ src/main/kotlin/net/bbo51dog/baitochan/Main.kt
@@ -3,11 +3,13 @@ package net.bbo51dog.baitochan
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jagrosh.jdautilities.command.CommandClientBuilder
 import net.bbo51dog.baitochan.command.StatusCommand
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
 
@@ -25,15 +27,19 @@ fun main() {
         .addEventListeners(commandClient)
         .build()
 
-    Executors.newScheduledThreadPool(1).scheduleAtFixedRate(
+    updateActivity(jda, Executors.newScheduledThreadPool(1))
+}
+
+fun updateActivity(jda: JDA, scheduler: ScheduledExecutorService) {
+    scheduler.schedule(
         {
             val connection = URL(StatusCommand.URL_BASE + StatusCommand.ADDRESS).openConnection() as HttpURLConnection
             connection.requestMethod = "GET"
             connection.connect()
             val data = jacksonObjectMapper().readTree(connection.inputStream)
             jda.presence.activity = Activity.playing(COMMAND_PREFIX + " - " + data.get("players").get("online").asText() + " Online Players")
+            updateActivity(jda, scheduler)
         },
-        0,
         1,
         TimeUnit.MINUTES
     )
